b38a5c9d64c50fb310a9734c115f0dfc1127f46a
Lewis Campbell <info@lewiscampbell.tech>
2024-12-21 13:11:54 +1300

    Used the submission/completion shared methods on the ring. This let's us remove the userspace queue vector.

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,13 @@ struct Socket {
     state: State,
 }
 
-fn main() -> io::Result<()> {
+// TCP Echo server
+// nc localhost 12345
+fn main() {
     // Initialize io_uring
-    let mut ring = IoUring::new(32)?;
+    let mut ring = IoUring::new(32).unwrap();
 
-    let handle = socket(AddressFamily::INET, SocketType::STREAM, Some(ipproto::TCP))?;
+    let handle = socket(AddressFamily::INET, SocketType::STREAM, Some(ipproto::TCP)).unwrap();
     let server = Socket {
         handle,
         buffer: [0u8; 1024],
@@ -32,9 +34,9 @@ fn main() -> io::Result<()> {
     };
 
     let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 12345);
-    set_socket_reuseaddr(&server.handle, true)?;
-    bind_v4(&server.handle, &addr)?;
-    listen(&server.handle, 128)?;
+    set_socket_reuseaddr(&server.handle, true).unwrap();
+    bind_v4(&server.handle, &addr).unwrap();
+    listen(&server.handle, 128).unwrap();
 
     let accept_e = opcode::Accept::new(
         types::Fd(server.handle.as_raw_fd()),
@@ -51,119 +53,102 @@ fn main() -> io::Result<()> {
     }
 
     loop {
-        ring.submit_and_wait(1)?;
+        ring.submit_and_wait(1).unwrap();
 
-        let mut done = false;
-        while !done {
-            // We have to use submission queue because of rust's borrow checker.
-            // Or else we have two mutable borrows of `ring` more than once.
-            // This way we collect all ops and
-            // submit them all at once at the end to the kernel.
-            let mut queue = Vec::new();
+        let mut sq = unsafe { ring.submission_shared() };
+        let cq = unsafe { ring.completion_shared() };
 
-            if let Some(cqe) = ring.completion().next() {
-                let client_ptr = cqe.user_data() as *mut Socket;
-                let client = unsafe { &mut *client_ptr };
+        for cqe in cq {
+            let client_ptr = cqe.user_data() as *mut Socket;
+            let client = unsafe { &mut *client_ptr };
 
-                if cqe.result() < 0 {
-                    eprintln!(
-                        "Error in state {:?}: {}",
-                        client.state,
-                        io::Error::from_raw_os_error(-cqe.result())
-                    );
-                    continue;
-                }
-
-                // Docs for opcodes: https://docs.rs/io-uring/latest/io_uring/opcode/index.html
-                match client.state {
-                    State::Accept => {
-                        // Create socket for clint connection
-                        let client_handle =
-                            unsafe { rustix::fd::OwnedFd::from_raw_fd(cqe.result()) };
-                        let client = Box::new(Socket {
-                            handle: client_handle,
-                            buffer: [0u8; 1024],
-                            state: State::Recv,
-                        });
-
-                        let client_ptr = Box::into_raw(client);
-
-                        // Prepare recv op
-                        let recv_e = opcode::Recv::new(
-                            types::Fd(unsafe { (*client_ptr).handle.as_raw_fd() }),
-                            unsafe { (*client_ptr).buffer.as_mut_ptr() },
-                            1024,
-                        )
-                        .build()
-                        .user_data(client_ptr as u64);
-
-                        // Prepare next accept
-                        let accept_e = opcode::Accept::new(
-                            types::Fd(server.handle.as_raw_fd()),
-                            ptr::null_mut(),
-                            ptr::null_mut(),
-                        )
-                        .build()
-                        .user_data(ptr::addr_of!(server) as u64);
-
-                        queue.push(recv_e);
-                        queue.push(accept_e);
-                    }
-                    State::Recv => {
-                        let read = cqe.result() as usize;
-                        if read == 0 {
-                            // Connection closed
-                            unsafe {
-                                let _ = Box::from_raw(client_ptr);
-                            }
-                            continue;
-                        }
-
-                        let received_message = String::from_utf8_lossy(&client.buffer[..read]);
-                        println!("Client sent: {}", received_message);
-
-                        client.state = State::Send;
-
-                        // Prepare send op
-                        let send_e = opcode::Send::new(
-                            types::Fd(client.handle.as_raw_fd()),
-                            client.buffer.as_ptr(),
-                            read as _,
-                        )
-                        .build()
-                        .user_data(client_ptr as u64);
-
-                        queue.push(send_e);
-                    }
-                    State::Send => {
-                        client.state = State::Recv;
-
-                        // Prepare next receive op
-                        let recv_e = opcode::Recv::new(
-                            types::Fd(client.handle.as_raw_fd()),
-                            client.buffer.as_mut_ptr(),
-                            1024,
-                        )
-                        .build()
-                        .user_data(client_ptr as u64);
-
-                        queue.push(recv_e);
-                    }
-                }
-            } else {
-                done = true;
+            if cqe.result() < 0 {
+                eprintln!(
+                    "Error in state {:?}: {}",
+                    client.state,
+                    io::Error::from_raw_os_error(-cqe.result())
+                );
+                continue;
             }
+            match client.state {
+                State::Accept => {
+                    // Create socket for clint connection
+                    let client_handle = unsafe { rustix::fd::OwnedFd::from_raw_fd(cqe.result()) };
+                    let client = Box::new(Socket {
+                        handle: client_handle,
+                        buffer: [0u8; 1024],
+                        state: State::Recv,
+                    });
 
-            // Submit all ops to the io_uring's queue which in turn submits them to the kernel.
-            for sqe in queue {
-                unsafe {
-                    ring.submission()
-                        .push(&sqe)
-                        .expect("submission queue is full");
+                    let client_ptr = Box::into_raw(client);
+
+                    // Prepare recv op
+                    let recv_e = opcode::Recv::new(
+                        types::Fd(unsafe { (*client_ptr).handle.as_raw_fd() }),
+                        unsafe { (*client_ptr).buffer.as_mut_ptr() },
+                        1024,
+                    )
+                    .build()
+                    .user_data(client_ptr as u64);
+
+                    // Prepare next accept
+                    let accept_e = opcode::Accept::new(
+                        types::Fd(server.handle.as_raw_fd()),
+                        ptr::null_mut(),
+                        ptr::null_mut(),
+                    )
+                    .build()
+                    .user_data(ptr::addr_of!(server) as u64);
+
+                    unsafe {
+                        sq.push_multiple(&[recv_e, accept_e]).unwrap();
+                    }
+                }
+                State::Recv => {
+                    let read = cqe.result() as usize;
+                    if read == 0 {
+                        // Connection closed
+                        unsafe {
+                            let _ = Box::from_raw(client_ptr);
+                        }
+                        continue;
+                    }
+
+                    let received_message = String::from_utf8_lossy(&client.buffer[..read]);
+                    println!("Client sent: {}", received_message);
+
+                    client.state = State::Send;
+
+                    // Prepare send op
+                    let send_e = opcode::Send::new(
+                        types::Fd(client.handle.as_raw_fd()),
+                        client.buffer.as_ptr(),
+                        read as _,
+                    )
+                    .build()
+                    .user_data(client_ptr as u64);
+
+                    unsafe {
+                        sq.push(&send_e).unwrap();
+                    }
+                }
+                State::Send => {
+                    client.state = State::Recv;
+
+                    // Prepare next receive op
+                    let recv_e = opcode::Recv::new(
+                        types::Fd(client.handle.as_raw_fd()),
+                        client.buffer.as_mut_ptr(),
+                        1024,
+                    )
+                    .build()
+                    .user_data(client_ptr as u64);
+
+                    unsafe {
+                        sq.push(&recv_e).unwrap();
+                    }
                 }
             }
         }
-
-        ring.submit()?;
     }
 }

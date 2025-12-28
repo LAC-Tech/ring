bcc5a347e726d3c51cabb112692f3c0d92d9fc30
Lewis Campbell <info@lewiscampbell.tech>
2025-12-28 19:49:35 +1300

    prep_read

--- src/prep.rs
+++ src/prep.rs
@@ -1,3 +1,5 @@
+use core::ffi::c_void;
+
 use rustix::io_uring::{io_uring_ptr, io_uring_sqe, IoringOp};
 
 pub unsafe fn nop(sqe: *mut io_uring_sqe) {
@@ -16,4 +18,20 @@ pub unsafe fn rw(
     (*sqe).addr_or_splice_off_in.addr = addr;
     (*sqe).len.len = len;
     (*sqe).off_or_addr2.off = offset;
+}
+
+pub unsafe fn read(
+    sqe: *mut io_uring_sqe,
+    fd: i32,
+    buffer: &mut [u8],
+    offset: u64,
+) {
+    rw(
+        sqe,
+        IoringOp::Read,
+        fd,
+        io_uring_ptr::new(buffer.as_mut_ptr() as *mut c_void),
+        buffer.len() as u32,
+        offset,
+    );
 }

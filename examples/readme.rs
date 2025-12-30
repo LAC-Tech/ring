2319319196276c4442d5c55f05dbf6b5c4b5d4b9
Lewis Campbell <info@lewiscampbell.tech>
2025-12-30 15:39:45 +1300

    changing comments in example to match tokio-rs

--- examples/readme.rs
+++ examples/readme.rs
@@ -11,9 +11,10 @@ fn main() {
     let mut buf = [0; 1024];
 
     let sqe = ring.get_sqe().unwrap();
+    prep::read(sqe, 0x42, fd.as_raw_fd(), &mut buf, 0);
+
     // Note that the developer needs to ensure
     // that the entry pushed into submission queue is valid (e.g. fd, buffer).
-    prep::read(sqe, 0x42, fd.as_raw_fd(), &mut buf, 0);
     let io_uring_cqe { user_data, res, .. } = unsafe {
         ring.submit_and_wait(1).expect("completion queue is empty");
         ring.copy_cqe().unwrap()
